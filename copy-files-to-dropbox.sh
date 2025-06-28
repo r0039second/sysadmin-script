1b7628378823424bd581365fcdbc80c876d63457
r0039second <tien.phanthe@gmail.com>
2025-06-28 10:24:45 +0700

    Create copy-files-to-dropbox.sh

---
+++ copy-files-to-dropbox.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp /home/tphanix/.config/google-chrome/Default/Bookmarks /home/tphanix/Dropbox/tien/bookmarkChrome/
+cp -r /home/tphanix/.ssh/* /home/tphanix/Dropbox/tien/sshconf/
+cp -r /home/tphanix/script/* /home/tphanix/Dropbox/moca/script/
+cp -r /home/tphanix/.bash* /home/tphanix/Dropbox/moca/bashprofile/
