188bbdbf2bc00dfa46d3823425e0055932b3957b
r0039second <tien.phanthe@gmail.com>
2025-06-28 11:07:43 +0700

    Create tomcat.sh

---
+++ tomcat.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+RE_PASS='CATALINA_OPTS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=9202 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false" CATALINA_BASE=/home/pass/tomcat-pass /opt/tomcat/bin/startup.sh'
